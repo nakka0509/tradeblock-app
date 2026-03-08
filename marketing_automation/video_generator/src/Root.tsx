d6bc8889e5106a52981a6c3c1d07a875c361b846
中川　翔太 <syota@nakkaMacBook-Air.local>
2026-03-08 20:09:25 +0900

    Add Chinese compositions and multi-account workflow

--- marketing_automation/video_generator/src/Root.tsx
+++ marketing_automation/video_generator/src/Root.tsx
@@ -170,6 +170,87 @@ export const RemotionRoot: React.FC = () => {
           videoSrc: 'assets/part3/ScreenRecording_03-08-2026 14-38-49_1.mov'
         }}
       />
+      {/* --- CHINESE (SIMPLIFIED) VERSIONS --- */}
+      <Composition
+        id="Feature1-TimeLock-CN"
+        component={TradeBlockCarousel}
+        durationInFrames={durationInFrames}
+        fps={fps}
+        width={1080}
+        height={1920}
+        defaultProps={{
+          hookText: '为什么90%的日内交易者都亏损？',
+          empathyText: '不是策略的问题，是大脑的"冲动模式"在作怪。',
+          appRevealText: '强制让自己离开市场。',
+          ctaText: '点击主页链接立即下载',
+          bgImageSrc1A: 'assets/part1/trading_despair_cn_1.png',
+          bgImageSrc1B: 'assets/part1/trading_despair_jp_1.png',
+          bgImageSrc2A: 'assets/part2/trading_loss_phone_1.png',
+          bgImageSrc2B: 'assets/part2/trading_despair_6.png',
+          bgImageSrc4: 'assets/part4/ScreenRecording_03-08-2026 14-36-25_1.mov',
+          videoSrc: 'assets/part3/ScreenRecording_03-08-2026 14-38-49_1.mov'
+        }}
+      />
+      <Composition
+        id="Feature2-TradeLog-CN"
+        component={TradeBlockCarousel}
+        durationInFrames={durationInFrames}
+        fps={fps}
+        width={1080}
+        height={1920}
+        defaultProps={{
+          hookText: '你真的能客观看待自己的交易吗？',
+          empathyText: '不直视亏损交易，就无法进步。',
+          appRevealText: '将所有记录汇总于此。',
+          ctaText: '点击主页链接立即下载',
+          bgImageSrc1A: 'assets/part1/trading_despair_cn_1.png',
+          bgImageSrc1B: 'assets/part1/trading_despair_5.png',
+          bgImageSrc2A: 'assets/part2/trading_despair_4.png',
+          bgImageSrc2B: 'assets/part2/trading_despair_2.png',
+          bgImageSrc4: 'assets/part4/ScreenRecording_03-08-2026 14-36-25_1.mov',
+          videoSrc: 'assets/part3/ScreenRecording_03-08-2026 14-38-49_1.mov'
+        }}
+      />
+      <Composition
+        id="Feature3-SecureProfit-CN"
+        component={TradeBlockCarousel}
+        durationInFrames={durationInFrames}
+        fps={fps}
+        width={1080}
+        height={1920}
+        defaultProps={{
+          hookText: '今天的盈利，你能守到明天吗？',
+          empathyText: '好不容易赚到的钱，别在深夜再次输掉。',
+          appRevealText: '保住盈利。强制锁定至次日早7点。',
+          ctaText: '点击主页链接立即下载',
+          bgImageSrc1A: 'assets/part1/trading_despair_cn_1.png',
+          bgImageSrc1B: 'assets/part1/trading_despair_jp_1.png',
+          bgImageSrc2A: 'assets/part2/trading_despair_3.png',
+          bgImageSrc2B: 'assets/part2/trading_despair_7.png',
+          bgImageSrc4: 'assets/part4/ScreenRecording_03-08-2026 14-36-25_1.mov',
+          videoSrc: 'assets/part3/ScreenRecording_03-08-2026 14-38-49_1.mov'
+        }}
+      />
+      <Composition
+        id="Feature4-Questionnaire-CN"
+        component={TradeBlockCarousel}
+        durationInFrames={durationInFrames}
+        fps={fps}
+        width={1080}
+        height={1920}
+        defaultProps={{
+          hookText: '不持仓就坐立难安？',
+          empathyText: '这就是过度交易。无谓的入场是毁掉账户的元凶。',
+          appRevealText: '解锁前，再问一次自己。',
+          ctaText: '点击主页链接立即下载',
+          bgImageSrc1A: 'assets/part1/trading_driving_man.png',
+          bgImageSrc1B: 'assets/part1/trading_despair_jp_1.png',
+          bgImageSrc2A: 'assets/part2/trading_loss_phone_1.png',
+          bgImageSrc2B: 'assets/part2/trading_despair_7.png',
+          bgImageSrc4: 'assets/part4/ScreenRecording_03-08-2026 14-36-25_1.mov',
+          videoSrc: 'assets/part3/ScreenRecording_03-08-2026 14-38-49_1.mov'
+        }}
+      />
     </>
   );
 };
